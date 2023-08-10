9f1c100bc16d483b08a26791b38f277fed5af757
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-08-10 23:04:36 +0900

    📑 :: monorepo 세팅

--- apps/teacher/src/pages/_app.tsx
+++ apps/teacher/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/reset.css";
+import "../styles/globalStyle.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
