29dfc4ac18f87c8d086d886de02dd286b68f6e43
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-09-06 19:30:03 +0900

    📝 :: teacher 출석확인 기능 개발

---
+++ apps/teacher/src/components/common/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const Sidebar = () => {
+  return (
+    <>
+      <S.SidebarWrap></S.SidebarWrap>
+    </>
+  );
+};
+
+export default Sidebar;
