760613c3c79daac11c3aa67bb081041766de6a0c
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-09-14 23:48:05 +0900

    ⚡ :: Auth 쿼리 추가

---
+++ apps/user/src/queries/Auth/Auth.query.ts
@@ -0,0 +1,8 @@
+import AuthRepositoryImpl from "@/repositories/AuthRepository/AuthRepositoryImpl";
+import { useMutation } from "react-query";
+
+export const usePostLoginMutation = () => {
+  const mutation = useMutation(AuthRepositoryImpl.login);
+
+  return mutation;
+};
