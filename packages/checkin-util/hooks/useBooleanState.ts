1edb5fcfaf8ad86ea90af5a656b835f3193496ee
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-09-19 14:35:33 +0900

    [Feat][Package] :: useBooleanState, useOutsideClick 생성

---
+++ packages/checkin-util/hooks/useBooleanState.ts
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react";
+
+export const useBooleanState = (initialValue?: boolean) => {
+  const [value, setValue] = useState(!!initialValue);
+
+  const setTrue = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  const toggle = useCallback(() => setValue((prev) => !prev), []);
+
+  return { value, setValue, setTrue, setFalse, toggle };
+};
---
+++ packages/checkin-util/hooks/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+export const useOutsideClick = (callback: () => void) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [callback]);
+
+  return ref;
+};
