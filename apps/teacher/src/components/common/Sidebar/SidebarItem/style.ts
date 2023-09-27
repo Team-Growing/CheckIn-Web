4a3ce00de273e987c3096aedeeb154c48bd3511b
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-09-27 22:51:45 +0900

    [Fix][Sidebar] : 사이드바 수정
    사이드바 수정

---
+++ apps/teacher/src/components/common/Sidebar/SidebarItem/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const NavItemWrap = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  row-gap: 18px;
+`;
+
+export const NavItemBox = styled.div<{ isMatch: boolean }>`
+  width: 260px;
+  height: 60px;
+
+  display: flex;
+  align-items: center;
+  padding-left: 57px;
+  column-gap: 17px;
+
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+  font-size: 20px;
+  background-color: ${({ isMatch }) => (isMatch == true ? "#00C537" : null)};
+  color: ${({ isMatch }) => (isMatch == true ? "#ffffff" : "#636363")};
+`;
+
+export const NavItemText = styled.p`
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+export const NavItemIcon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
--- apps/teacher/src/components/common/Sidebar/index.tsx
+++ apps/teacher/src/components/common/Sidebar/index.tsx
@@ -1,12 +1,26 @@
-import React from "react";
 import * as S from "./style";
+import logo from "../../../assets/Logo/logo.svg";
+import NavItem from "./SidebarItem/index";
+import NavUser from "./";
+import { useRouter } from "next/router";
 
-const Sidebar = () => {
+const Nav = () => {
+  const router = useRouter();
   return (
-    <>
-      <S.SidebarWrap></S.SidebarWrap>
-    </>
+    <S.NavContainer>
+      <S.NavTopWrap>
+        <S.NavLogo
+          src={logo.src}
+          width={125}
+          height={38}
+          alt="메인 로고"
+          onClick={() => router.push("/")}
+        />
+        <NavItem />
+      </S.NavTopWrap>
+      <NavUser />
+    </S.NavContainer>
   );
 };
 
-export default Sidebar;
+export default Nav;
