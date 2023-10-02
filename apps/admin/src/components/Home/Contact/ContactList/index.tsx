083f3c6d009c2895bee4461273873d9c164d49d9
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-02 18:35:08 +0900

    [Feat][Admin] :: 담당자 문의 퍼블리싱 1차

--- apps/admin/src/components/Home/Contact/ContactList/index.tsx
+++ apps/admin/src/components/Home/Contact/ContactList/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import * as S from "./style";
 
 const ContactList = () => {
   return (
-    <table>
-      <tr>
-        <td>일자</td>
-        <td>문의 내용</td>
-        <td>이름</td>
-      </tr>
-    </table>
+    <>
+      <S.ContactListContainer>
+        <S.ContactListTitle>
+          <p>일자</p>
+          <p>문의 내용</p>
+          <p>이름</p>
+        </S.ContactListTitle>
+        {Array.from({ length: 10 }).map(() => (
+          <S.ContactListText key={`df`}>
+            <p>09/13</p>
+            <p>[방과후 교재 지원 문의] 플룻교재 관련건</p>
+            <p>백승하</p>
+          </S.ContactListText>
+        ))}
+      </S.ContactListContainer>
+    </>
   );
 };
 
