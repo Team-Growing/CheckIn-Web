8bed312d2f5d4afc5632f260a112427d36f9a261
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-04 23:36:37 +0900

    [Feat][User] :: 활성화 공지 불러오기

--- apps/user/src/components/Home/Notifiction/index.tsx
+++ apps/user/src/components/Home/Notifiction/index.tsx
@@ -1,20 +1,34 @@
-import { Card, CardTitle } from "@checkin/ui";
-import Image from "next/image";
-import React from "react";
-import NotifictionImg from "../../../assets/Icon/Notifiction.svg";
+import { Card, CardTitle, Flex } from "@checkin/ui";
+import React, { useMemo } from "react";
 import { NotifictionIcon } from "@checkin/icon";
+import { useGetActiveNoticeQuery } from "@/queries/Notice/query";
+import Slider from "react-slick";
+import { NoticeContainer, NoticeContent } from "./style";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Notifiction = () => {
+  const { data } = useGetActiveNoticeQuery();
+
+  const noticeSetting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrow: false,
+  };
+
   return (
-    <Card
-      type="Notifiction"
-      customStyle={{ display: "flex", columnGap: "63px" }}
-    >
+    <NoticeContainer>
       <CardTitle>
         <NotifictionIcon />
         공지사항
       </CardTitle>
-    </Card>
+      <NoticeContent>
+        {"오늘 테니스 방과후는 방과후 강사님 사정으로 휴강합니다!"}
+      </NoticeContent>
+    </NoticeContainer>
   );
 };
 
---
+++ apps/user/src/components/Home/Notifiction/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const NoticeContainer = styled.div`
+  width: 92.5%;
+  height: 6vh;
+
+  display: flex;
+  align-items: center;
+
+  position: relative;
+  overflow: hidden;
+  column-gap: 63px;
+  padding-left: 29px;
+
+  border-radius: 5px;
+  border: 1px solid #cdd0dc;
+  background: #fff;
+`;
+
+export const NoticeContent = styled.div`
+  color: #828282;
+  font-size: 18px;
+  font-weight: 500;
+`;
