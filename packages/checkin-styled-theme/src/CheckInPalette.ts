a771c93b85ae8bd043a2e1c74de7a60c4666ff89
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-22 22:10:06 +0900

    [Fix][Package] :: CheckInColor 추가

---
+++ packages/checkin-styled-theme/src/CheckInPalette.ts
@@ -0,0 +1,28 @@
+export const CheckInColor = {
+  Text: {
+    Lighter: "#FFF",
+    Light: "#6C7072",
+    Base: "#404446",
+    Dark: "#303437",
+    Darker: "#202325",
+    Darkest: "#090A0A",
+  },
+  Main: {
+    Base: "#00C537",
+    Dark: "#008325",
+  },
+  Background: {
+    Lightest: "#FFF",
+    Lightbase: "#F5F5F5",
+    Lightdark: "#F2F2F2",
+    DarkLighter: "#454749",
+    DarkLight: "#454749",
+    Darkbase: "#090A0A",
+  },
+  Category: {
+    Blue: "#0073C9",
+    Yello: "#F7D356",
+    Green: "#209915",
+    Red: "#DE4B41",
+  },
+} as const;
