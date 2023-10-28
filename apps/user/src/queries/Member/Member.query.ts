17ae99879a869144534cb3aac6463b7e9a2d8cd8
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-28 15:21:37 +0900

    [Feat][User] :: 내 강좌 query 작성

--- apps/user/src/queries/Member/Member.query.ts
+++ apps/user/src/queries/Member/Member.query.ts
@@ -5,17 +5,31 @@ import { AxiosError } from "axios";
 import { MemberType } from "@checkin/types";
 import MemberRepositoryImpl from "@/repositories/MemberRepository/MemberRepositoryImpl";
 import Token from "@/libs/token/Token";
-
+import { CheckInQueryKey } from "@checkin/querykey";
 export const useGetMemberInfo = (
   options?: UseQueryOptions<MemberType, AxiosError, MemberType, string>
 ) =>
-  useQuery(QueryKey.member.getMy, () => MemberRepositoryImpl.getMemberInfo(), {
-    cacheTime: 1000 * 60 * 60,
-    staleTime: 1000 * 60 * 5,
-    onError: () => {
-      CheckinToast.showError("토근이 위조되었습니다");
-      Token.clearToken();
-      window.location.href = "/sign";
-    },
-    ...options,
-  });
+  useQuery(
+    CheckInQueryKey.member.getMy,
+    () => MemberRepositoryImpl.getMemberInfo(),
+    {
+      cacheTime: 1000 * 60 * 60,
+      staleTime: 1000 * 60 * 5,
+      onError: () => {
+        CheckinToast.showError("토근이 위조되었습니다");
+        Token.clearToken();
+        window.location.href = "/sign";
+      },
+      ...options,
+    }
+  );
+
+export const useGetMemberLectures = () =>
+  useQuery(
+    CheckInQueryKey.member.getMyLectures,
+    () => MemberRepositoryImpl.getMemberLectures(),
+    {
+      cacheTime: 1000 * 60 * 60,
+      staleTime: 1000 * 60 * 5,
+    }
+  );
