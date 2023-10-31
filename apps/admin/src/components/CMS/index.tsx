89809d7630aba7738876e56a290fa7283d13749a
백승하 <qortmdgk12@naver.com>
2023-10-31 20:09:26 +0900

    [Refactor][Admin] :: CMS 페이지 SSR 적용 (#116)

--- apps/admin/src/components/CMS/index.tsx
+++ apps/admin/src/components/CMS/index.tsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
-import { Flex, SectionHeader } from "@checkin/ui";
+import { Button, ButtonWrapper, Flex, SectionHeader } from "@checkin/ui";
 import { CmsStatusList } from "./constant";
 import LecturesList from "./CMSList";
 import useManageLecture from "@/hooks/Lecture/useManageLecture";
+import { useGetAllLecturesQuery } from "@/queries/Lecture/query";
+import { useRouter } from "next/router";
 
 const CMS = () => {
+  const { query } = useRouter();
+  const [grade, setGrade] = useState(Number(query.grade));
+  const router = useRouter();
+
   const {
     lectureIdList,
     onChangeLectureIdList,
     onChangeLectureStatus,
     onPatchLectureStatus,
   } = useManageLecture();
-  console.log(lectureIdList);
 
   return (
     <S.CMSContainer>
       <S.CMSTopWrap>
         <SectionHeader title="방과후 관리" subTitle="Lecture Manage" />
       </S.CMSTopWrap>
+      <Flex customStyle={{ width: "100%", paddingLeft: "104px" }}>
+        <ButtonWrapper>
+          <Button
+            customStyle={{ width: "138px" }}
+            type="primary"
+            isSelect={grade === 1 ? false : true}
+            onClick={() => {
+              setGrade(1);
+              router.push("/cms/1");
+            }}
+          >
+            1학년
+          </Button>
+          <Button
+            customStyle={{ width: "138px" }}
+            type="primary"
+            isSelect={grade === 2 ? false : true}
+            onClick={() => {
+              setGrade(2);
+              router.push("/cms/2");
+            }}
+          >
+            2학년
+          </Button>
+        </ButtonWrapper>
+      </Flex>
       <S.CMSListContainer>
         {CmsStatusList.map((item, key) => (
           <Flex
@@ -39,6 +70,7 @@ const CMS = () => {
             </S.CmsStatusBox>
             <LecturesList
               status={item.status}
+              grade={grade}
               lectureIdList={lectureIdList}
               onChangeLectureIdList={onChangeLectureIdList}
             />
