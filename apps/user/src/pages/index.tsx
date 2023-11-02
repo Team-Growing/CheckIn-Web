5895fa61293fbe2abf63d17bca483e8f1ec3fa01
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-03 01:46:16 +0900

    Chore :: hoc 제거

--- apps/user/src/pages/index.tsx
+++ apps/user/src/pages/index.tsx
@@ -1,15 +1,13 @@
-import withAuth from "@/components/HOC/withAuth";
 import Home from "@/components/Home";
 import Head from "next/head";
 
 export default function HomePage() {
-  const AuthHomePage = withAuth(Home);
   return (
     <>
       <Head>
         <title>홈 | check-in</title>
       </Head>
-      <AuthHomePage />
+      <Home />
     </>
   );
 }
