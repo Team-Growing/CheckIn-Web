3107b149fff8f2df496d31d705fe9f800ac323aa
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-02 16:06:50 +0900

    [Test][Package] :: 배포 test

--- packages/checkin-ui/.storybook/main.js
+++ packages/checkin-ui/.storybook/main.js
@@ -3,5 +3,13 @@ module.exports = {
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
 };
