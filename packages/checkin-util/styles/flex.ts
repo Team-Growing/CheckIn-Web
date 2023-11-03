28568601503e1455fae2f303e6e5370ca8c8b6d6
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-03 10:27:03 +0900

    chore(checkin-util) : styled-components 추가

--- packages/checkin-util/styles/flex.ts
+++ packages/checkin-util/styles/flex.ts
@@ -1,5 +1,5 @@
-import { css } from "styled-components";
 import type { CSSProperties } from "react";
+import { css } from "styled-components";
 
 interface PropsType {
   flexDirection?: CSSProperties["flexDirection"];
