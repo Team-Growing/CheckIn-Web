af6705505a3042c97d74e5c81710a6cda2d248ef
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-14 17:04:16 +0900

    chore : 삭제시 주의 표시

--- apps/admin/src/hooks/Notice/useDeleteNotice.ts
+++ apps/admin/src/hooks/Notice/useDeleteNotice.ts
@@ -9,16 +9,20 @@ const useDeleteNotice = () => {
   const deleteNoticeMutation = useDeleteNoticeMutation();
 
   const onDeleteNotice = (id: string) => {
-    deleteNoticeMutation.mutate(id, {
-      onSuccess: () => {
-        CheckinToast.showSuccess("공지가 삭제되었습니다.");
-        queryClient.invalidateQueries(CheckInQueryKey.notice.getAll);
-      },
-      onError: () => {
-        CheckinToast.showError("error...");
-      },
-    });
+    const confirm = window.confirm("공지를 삭제하시겠습니까");
+    if (confirm) {
+      deleteNoticeMutation.mutate(id, {
+        onSuccess: () => {
+          CheckinToast.showSuccess("공지가 삭제되었습니다.");
+          queryClient.invalidateQueries(CheckInQueryKey.notice.getAll);
+        },
+        onError: () => {
+          CheckinToast.showError("error...");
+        },
+      });
+    }
   };
+
   return { onDeleteNotice };
 };
 
