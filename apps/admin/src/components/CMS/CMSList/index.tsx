d4c6946e074fbbcea2a684672b8f05532f7c670c
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-15 14:27:41 +0900

    refactor : 방과후 관리 리팩토링

--- apps/admin/src/components/CMS/CMSList/index.tsx
+++ apps/admin/src/components/CMS/CMSList/index.tsx
@@ -1,6 +1,6 @@
 import { LectureTagBox } from "@checkin/ui";
 import React from "react";
-import { useGetLecturesByStatus } from "@/queries/Lectures/query";
+import { useGetAllLecturesQuery } from "@/queries/Lectures/query";
 
 interface Props {
   status: string;
@@ -15,24 +15,28 @@ const LecturesList = ({
   lectureIdList,
   grade,
 }: Props) => {
-  const { data } = useGetLecturesByStatus({ status, grade });
+  const { data } = useGetAllLecturesQuery(grade);
 
   return (
     <>
-      {data?.data.map((data) => (
-        <LectureTagBox
-          key={data.lectureId.value}
-          grade={data.acceptableStudent.targetGrade}
-          lectureTag={data.lectureTag}
-          people={data.enrollStudent}
-          place={data.placeType}
-          teacher={data.lectureTeacher.name}
-          title={data.lectureName}
-          isSelect={lectureIdList.includes(data.lectureId.value) ? true : false}
-          onSelect={() => onChangeLectureIdList(data.lectureId.value)}
-          type="Enrol"
-        />
-      ))}
+      {data?.data
+        .filter((lecture) => lecture.lectureStatus === status)
+        .map((data) => (
+          <LectureTagBox
+            key={data.lectureId.value}
+            grade={data.acceptableStudent.targetGrade}
+            lectureTag={data.lectureTag}
+            people={data.enrollStudent}
+            place={data.placeType}
+            teacher={data.lectureTeacher.name}
+            title={data.lectureName}
+            isSelect={
+              lectureIdList.includes(data.lectureId.value) ? true : false
+            }
+            onSelect={() => onChangeLectureIdList(data.lectureId.value)}
+            type="Enrol"
+          />
+        ))}
     </>
   );
 };
