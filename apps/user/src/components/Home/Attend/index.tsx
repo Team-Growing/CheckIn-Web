844fc5c5e42e0bf0f0d5bdf90a5ce7165af03b19
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-11-26 12:33:20 +0900

    refactor(user) : 출석코드 입력부분 오늘 내 강좌 ssr 적용 빈 배열 처리및

--- apps/user/src/components/Home/Attend/index.tsx
+++ apps/user/src/components/Home/Attend/index.tsx
@@ -4,6 +4,7 @@ import { CalendarIcon } from "@checkin/icon";
 import useAttendance from "@/hooks/Attendance/useAttendance";
 import AttendList from "./AttendList";
 import AttendLectureForm from "./AttendLectureForm";
+import { useGetTodayMyLecturesQuery } from "@/queries/Lectures/query";
 
 const Attend = () => {
   const {
@@ -13,20 +14,30 @@ const Attend = () => {
     onClickSetId,
     onChangeAttendanceCode,
   } = useAttendance();
+  const { data } = useGetTodayMyLecturesQuery();
+
   return (
     <Card type="Enrol">
       <CardTitle>
         <CalendarIcon />
         출석코드 입력
       </CardTitle>
-      <Flex gap={20} customStyle={{ width: "100%", height: "100%" }}>
-        <AttendList lectureId={lectureId} onClickSetId={onClickSetId} />
-        <AttendLectureForm
-          attendanceCode={attendanceCode}
-          onAttendanceLecture={onAttendanceLecture}
-          onChangeAttendanceCode={onChangeAttendanceCode}
-        />
-      </Flex>
+      {data?.data.length === 0 ? (
+        <>오늘은 방과후가 없습니다</>
+      ) : (
+        <Flex gap={20} customStyle={{ width: "100%", height: "100%" }}>
+          <AttendList
+            data={data!}
+            lectureId={lectureId}
+            onClickSetId={onClickSetId}
+          />
+          <AttendLectureForm
+            attendanceCode={attendanceCode}
+            onAttendanceLecture={onAttendanceLecture}
+            onChangeAttendanceCode={onChangeAttendanceCode}
+          />
+        </Flex>
+      )}
     </Card>
   );
 };
--- apps/user/src/queries/Lectures/query.ts
+++ apps/user/src/queries/Lectures/query.ts
@@ -57,7 +57,7 @@ export const useGetEnrolmentLecturesQuery = (
 
 export const useGetTodayMyLecturesQuery = () =>
   useQuery(
-    "lectures/todayMyLectures",
+    CheckInQueryKey.lecture.getTodayMyLectures,
     () => LectureRepositoryImpl.getTodayMyLectures(),
     {
       staleTime: 1000 * 60 * 60,
