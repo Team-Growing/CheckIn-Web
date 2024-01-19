447a96f8530286171a9c5182f33e1cb829a3f42b
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-01-19 13:54:25 +0900

    chore(user) : Nav 경로 매치 수정

--- apps/user/src/components/common/Nav/NavItem/index.tsx
+++ apps/user/src/components/common/Nav/NavItem/index.tsx
@@ -5,15 +5,21 @@ import { usePathname } from "next/navigation";
 
 const NavItem = () => {
   const pathname = usePathname();
+
+  const isPathMatch = (path: string, currentPath: string): boolean => {
+    const regex = new RegExp(`^${path.replace(/\[.*\]/, "\\d+")}$`);
+    return regex.test(currentPath);
+  };
+
   return (
     <S.NavItemWrap>
       {NAV_ITEM.map((data, idx) => (
         <Link key={idx} href={data.path}>
           <S.NavItemBox
-            isMatch={data.pathname == pathname ? true : false}
+            isMatch={isPathMatch(data.pathname, pathname)}
             key={idx}
           >
-            <data.icon isMatch={data.pathname == pathname ? true : false} />
+            <data.icon isMatch={isPathMatch(data.pathname, pathname)} />
             <S.NavItemText>{data.title}</S.NavItemText>
           </S.NavItemBox>
         </Link>
