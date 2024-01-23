e2491b2d8ffc0a2b98828219f0d140ecea71117c
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-01-23 11:27:28 +0900

    chore(user) : config 수정

--- apps/user/next.config.js
+++ apps/user/next.config.js
@@ -2,37 +2,13 @@
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
-const ENV = {
-  ENV: process.env.NODE_ENV ?? "",
-  API_HOST: process.env.API_HOST ?? process.env.NEXT_PUBLIC_BASE_URL,
-};
-
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@checkin/ui"],
   swcMinify: true,
-  sentry: {
-    hideSourceMaps: true,
-    tunnelRoute: "/monitoring-tunnel",
-    transpileClientSDK: true,
-  },
-  publicRuntimeConfig: { ...ENV },
 };
 
-const sentryWebpackPluginOptions = {
-  org: "checkin",
-  project: "checkin-user",
-  slient: true,
-};
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-
-// Injected content via Sentry wizard below
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
+module.exports = nextConfig;
 
 module.exports = withSentryConfig(
   module.exports,
@@ -42,7 +18,7 @@ module.exports = withSentryConfig(
 
     // Suppresses source map uploading logs during build
     silent: true,
-    org: "check-in-3e",
+    org: "team-growing",
     project: "checkin-user",
   },
   {
